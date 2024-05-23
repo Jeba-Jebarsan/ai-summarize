8c9af7547e43b323dc9ef663fc45ea2ed8b6f97b
Thomas Jebarsan <142016100+Jeba-Jebarsan@users.noreply.github.com>
2024-05-23 12:16:12 +0530

    first commit

---
+++ src/components/Hero.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+import { logo } from "../assets";
+
+const Hero = () => {
+  return (
+    <header className='w-full flex justify-center items-center flex-col'>
+      <nav className='flex justify-between items-center w-full mb-10 pt-3'>
+        <img src={logo} alt='sumz_logo' className='w-28 object-contain' />
+      </nav>
+
+      <h1 className='head_text'>
+        Summarize Articles using <br className='max-md:hidden' />
+        <span className='orange_gradient '>Minify</span>
+      </h1>
+      <h2 className='desc'>
+      Minify Your solution for streamlined reading. Harnessing AI technology, Minify distills lengthy articles into crisp, digestible summaries, making information accessible and easy to comprehend
+
+      </h2>
+    </header>
+  );
+};
+
+export default Hero;
