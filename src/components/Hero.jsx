9a54fdefc19fac906d9dbaeb3be5988448e92674
Thomas Jebarsan <142016100+Jeba-Jebarsan@users.noreply.github.com>
2024-07-13 07:56:22 +0530

    logo

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -10,7 +10,7 @@ const Hero = () => {
       </nav>
 
       <h1 className='head_text'>
-        Summarize Articles with <br className='max-md:hidden' />
+      Condense articles using<br className='max-md:hidden' />
         <span className='orange_gradient '>Minify</span>
       </h1>
       <h2 className='desc'>
